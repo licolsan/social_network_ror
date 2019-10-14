7e7b4ab396e86aff4f2fbd5c7bb177fb3b162161
licolsan <phamhoanglinh198@gmail.com>
2019-10-14 15:45:47 +0700

    [LinhPH] Separate 'edit' & 'destroy' permission on comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,8 @@ class CommentsController < ApplicationController
   before_action :get_services
   before_action :find_post, only: [ :edit, :create, :destroy ]
   before_action :find_comment, except: [ :create ]
-  before_action :is_permitted, only: [ :edit, :update, :destroy ]
+  before_action :is_editable, only: [ :edit, :update ]
+  before_action :is_destroyable, only: [ :destroy ]
 
   def show
   end
@@ -46,8 +47,15 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:content, :image)
   end
 
-  def is_permitted
-    if !@comment_service.is_permitted(current_user, @comment)
+  def is_editable
+    if !@comment_service.is_owner(current_user, @comment)
+      flash[:notice] = "You are not allow to edit on this comment"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def is_destroyable
+    if !@comment_service.is_destroyable(current_user, @comment)
       flash[:notice] = "You are not permited on this comment"
       redirect_back(fallback_location: root_path)
     end
